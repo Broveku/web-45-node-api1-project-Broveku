4cd14a72a97901a40db881cf45c60669e613dfb8
Broveku <gearhearjordan@gmail.com>
2021-08-30 13:07:15 -0700

    wip

--- api/server.js
+++ api/server.js
@@ -1,3 +1,72 @@
 // BUILD YOUR SERVER HERE
+const express = require('express')
+const User = require('./users/model')
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express()
+
+server.use(express.json())
+
+server.post('/api/users', (req, res)=>{
+    const newUser = req.body
+    User.insert(newUser)
+        .then(user=>{
+            res.status(201).json(user)
+        })
+        .catch(err=>{
+            res.status(400).json({message:"Please provide name and bio for the user"})
+        })
+})
+
+server.get('/api/users', (req, res)=>{
+    User.find()
+        .then(users=>{
+            res.status(200).json(users)
+        })
+        .catch(err=>{
+            res.status(500).json({message: "The users information could not be retrieved"})
+        })
+})
+
+server.get('/api/users/:id', async (req, res)=>{
+    User.findById(req.params.id)
+        .then(user =>{
+            if(user){
+                res.status(200).json(user)
+            }else{
+                res.status(404).json({message: "The user with the specified ID does not exist"})
+            }
+        })
+        .catch(err=>{
+            res.status(500).json({message: "The user information could not be retrieved"})
+        })
+})
+
+server.delete('/api/users/:id', (req, res)=>{
+    User.remove(req.params.id)
+        .then(user =>{
+           if(user){
+               res.status(200).json(user)
+           } else{
+               res.status(404).json({message: "The user with the specified ID does not exist"})
+           }
+        })
+        .catch(err=>{
+            res.status(500).json({message: "The user could not be removed"})
+        })
+})
+
+server.put('/api/users/:id', async (req, res)=>{
+    const { id } = req.params
+    const changes = req.body
+    try{
+        const result = await User.update(id, changes)
+        res.status(200).json(result)
+
+    }catch (err){
+        console.log(err)
+        res.status(500).json({message: err.message})
+    }
+})
+
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
